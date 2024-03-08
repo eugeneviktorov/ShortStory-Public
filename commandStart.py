798313ec3e6b9e4f2bb3c1e4eb5313f49191d1ee
Eugene <e.viktorovvv@gmail.com>
2024-03-08 12:07:38 +0600

    Обновление кода

--- commandStart.py
+++ commandStart.py
@@ -17,31 +17,25 @@ async def startCommandHandler(message: types.Message, state: FSMContext):
     adminData = cursor.fetchone()
     # Пользователь является администратором
     if adminData is not None:
-        # Проверка, является ли администратор пользователем
-        cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
-        user_data = cursor.fetchone()
-        # Пользователь присутвует в базе данных, выдача клавиатуры для администратора
-        if user_data is not None:
-            photo = open('resources/images/mainMenu.png', 'rb')
-            await message.answer_photo(photo=photo, caption="Главное меню", reply_markup=choosingStoryActionAdminKB)
-        # Пользователь отсутсвует в базе данных, добавление и приветствие
-        else:
-            cursor.execute('INSERT INTO users (user_id) VALUES (?)', (user_id,))
-            conn.commit()
-            photo = open('resources/images/newUser.png', 'rb')
-            await message.answer_photo(photo=photo, caption="Бот ShortStory приветствует вас!\nВы в главном меню", reply_markup=choosingStoryActionAdminKB)
+        keyboard = choosingStoryActionAdminKB
+        await userDataStarting(message, state, user_id, keyboard)
     # Пользователь не является администратором
     else:
-        # Проверка, является ли пользователь пользователем
-        cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
-        user_data = cursor.fetchone()
-        # Пользователь присутвует в базе данных, выдача клавиатуры для пользователя
-        if user_data is not None:
-            photo = open('resources/images/mainMenu.png', 'rb')
-            await message.answer_photo(photo=photo, caption="Главное меню", reply_markup=choosingStoryActionKB)
-        # Пользователя отсутсвует в базе данных, добавление и приветствие
-        else:
-            cursor.execute('INSERT INTO users (user_id) VALUES (?)', (user_id,))
-            conn.commit()
-            photo = open('resources/images/newUser.png', 'rb')
-            await message.answer_photo(photo=photo, caption="Бот ShortStory приветствует вас!\nВы в главном меню", reply_markup=choosingStoryActionKB)
+        keyboard = choosingStoryActionKB
+        await userDataStarting(message, state, user_id, keyboard)
+
+# Выдача сообщения пользователю на основе его статуса
+async def userDataStarting(message: types.Message, state: FSMContext, user_id, keyboard):
+    # Проверка, является ли пользователь пользователем
+    cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
+    userData = cursor.fetchone()
+    # Пользователь присутвует в базе данных, выдача клавиатуры для пользователя
+    if userData is not None:
+        photo = open('resources/images/mainMenu.png', 'rb')
+        await message.answer_photo(photo=photo, caption="Главное меню", reply_markup=keyboard)
+    # Пользователя отсутсвует в базе данных, добавление и приветствие
+    else:
+        cursor.execute('INSERT INTO users (user_id) VALUES (?)', (user_id,))
+        conn.commit()
+        photo = open('resources/images/newUser.png', 'rb')
+        await message.answer_photo(photo=photo, caption="Бот ShortStory приветствует вас!\nВы в главном меню", reply_markup=keyboard)
